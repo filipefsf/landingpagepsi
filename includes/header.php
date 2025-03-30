7226173dbc44568aeeddd504f8d45b9e9be89782
filipefsf <sfilipe41@outlook.com>
2025-03-30 17:06:30 -0300

    First commit

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Psicologia - Ana Paula Assis</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include 'includes/header.php'; ?>
+
+<!--Painel inicial de chamada à ação-->
+<section class="hero-section">
+    <div class="background-blur"></div> <!-- Imagem de fundo desfocada -->
+    <div class="overlay"></div> <!-- Overlay escuro para melhorar a leitura -->
+
+    <div class="main-section-1">
+        <div class="container container-presentation">
+            <h4 class="display-6">ANA PAULA ASSIS</h4>
+            <h1 class="display-4 fw-bold">Atendimento psicológico</h1>
+            <p> Cuide da sua <b>saúde mental</b> com um atendimento psicológico especializado. 
+                            Agende sua sessão de psicoterapia e comece hoje mesmo sua jornada para 
+                            uma vida mais equilibrada e saudável!
+            </p>
+            <a href="#" class="btn btn-primary btn-lg btn-action-contato fw-bold display-4">Agendar consulta</a>
+        </div>
+
+        <div class="container container-img">
+            <img src="assets/img/mulher_exemplo.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="hero-section hero-section-area-atuacao">
+    <div class="main-section-2">
+        <div class="container container-area-atuacao">
+            <h2 class="display-6 fw-bold">
+                Áreas de atuação
+            </h2>
+            <p> Nas nossas sessões, meu objetivo é te ajudar a entender e lidar melhor com suas emoções, 
+                de uma forma tranquila e prática. Quero te passar ferramentas que realmente ajudem 
+                no seu crescimento emocional.
+            </p>
+        </div>
+
+        <div class="container container-areas">
+            <button class="btn btn-secondary" disabled><i class="fa-solid fa-head-side-virus"></i>Ansiedade</button>
+            <button class="btn btn-secondary" disabled><i class="fa-solid fa-face-sad-tear"></i>Depressão</button>
+            <button class="btn btn-secondary" disabled><i class="fa-solid fa-heart-crack"></i>Relacionamento</button>
+            <button class="btn btn-secondary" disabled><i class="fa-solid fa-hands-holding-child"></i>Psicologia Infantil</button>
+        </div>
+
+    </div>
+</section>
+
+
+<?php include 'includes/footer.php'; ?>
